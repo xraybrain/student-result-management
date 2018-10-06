f404b2e8897caf7345f081865fa342f7f5f5848e
xraybrain <xraybrain@gmail.com>
2018-10-06 03:42:52 +0100

    database configured for production

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 let mongoURI = "";
 
 if(process.env.NODE_ENV === "production"){
-  mongoURI = "";
+  mongoURI = "mongodb://xraybrain:jude6053@ds125073.mlab.com:25073/resultmanagement-prod";
 } else {
   mongoURI = "mongodb://127.0.0.1:27017/resultManagement";
 }
